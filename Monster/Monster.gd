13665990079bdc4c916d906f6043c53a1cff835b
santosty25 <santosty25@up.edu>
2024-11-01 19:50:05 -0700

    Fixed Main and Monster
    * Monster now chases Player
    * Main has NavigationLink and Light Source

--- Monster/Monster.gd
+++ Monster/Monster.gd
@@ -1,22 +1,22 @@
 class_name Monster
 extends CharacterBody3D
 
-@onready var navigation: NavigationAgent3D = $NavigationAgent3D
-@onready var faceDir: Node3D = $FaceDirection
+@onready var navigation= $NavigationAgent3D
+@onready var faceDir = $FaceDirection
 
-const speed = 5.0
-const turnSpeed = 4.0
+const SPEED = 5.0
+@export var turnSpeed = 4.0
 
-var player: CharacterBody3D = null
+var player
 
 func _ready():
 	# get player node
-	player = get_parent().get_node("Player")
+	player = get_tree().get_nodes_in_group("Player")[0]
 
 func _physics_process(delta):
 	faceDir.look_at(player.global_transform.origin, Vector3.UP)
 	rotate_y(deg_to_rad(faceDir.rotation.y * turnSpeed))
-	navigation.set_target_postion(player.global_transform.origin)
-	var velocity = (navigation.get_next_path_position() - transform.origin).normalized() * speed * delta
+	navigation.set_target_position(player.global_transform.origin)
+	var velocity = (navigation.get_next_path_position() - transform.origin).normalized() * SPEED * delta
 	move_and_collide(velocity)
 	
