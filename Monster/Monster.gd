0c6db90fa3cdad5b5d4d9163a0e31dd8096461f4
santosty25 <santosty25@up.edu>
2024-11-01 21:05:35 -0700

    Partial Monster Running Away Code

--- Monster/Monster.gd
+++ Monster/Monster.gd
@@ -5,6 +5,9 @@ extends CharacterBody3D
 @onready var faceDir = $FaceDirection
 
 const SPEED = 1.0
+const fleeDistance = 10.0
+const fleeTime = 5.0
+
 @export var turnSpeed = 4.0
 @export var damageInt = 1.0
 @export var damage = 5.0
@@ -12,23 +15,37 @@ const SPEED = 1.0
 var player
 var damageTimer = 0.0
 var isPlayerIn = false
+var flee = false
 
 func _ready():
 	# get player node
 	player = get_tree().get_nodes_in_group("Player")[0]
 
 func _physics_process(delta):
+	
+	if player:
+		if player.inside:
+			runAway(delta)
+		else:
+			chasePlayer(delta)
+
+	if isPlayerIn:
+		damagePlayer(delta)
+
+func chasePlayer(delta):
 	faceDir.look_at(player.global_transform.origin, Vector3.UP)
 	rotate_y(deg_to_rad(faceDir.rotation.y * turnSpeed))
 	navigation.set_target_position(player.global_transform.origin)
 	var velocity = (navigation.get_next_path_position() - transform.origin).normalized() * SPEED * delta
 	move_and_collide(velocity)
-	
-	if isPlayerIn:
-		damagePlayer(delta)
-	
 
-
+func runAway(delta):
+	var runDir = (global_transform.origin - player.global_transform.origin).normalized()
+	
+	var runVelocity = runDir * SPEED * delta
+	move_and_collide(runVelocity)
+	
+	
 func _on_attack_region_body_entered(body):
 	if body.is_in_group("Player"):
 		isPlayerIn = true
@@ -38,7 +55,6 @@ func _on_attack_region_body_exited(body):
 	if body.is_in_group("Player"):
 		isPlayerIn = false
 		print("Player out of range")
-	
 
 func damagePlayer(delta):
 	damageTimer -= delta
@@ -46,4 +62,6 @@ func damagePlayer(delta):
 		# reset timer
 		damageTimer = damageInt
 		player.takeDamage(damage)
-		
+
+func despawn():
+	queue_free()
