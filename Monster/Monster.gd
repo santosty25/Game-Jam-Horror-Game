21498a54e130ce5a3e75414095b3284d757a04ae
santosty25 <santosty25@up.edu>
2024-11-01 20:11:07 -0700

    Semi Added Damage Functions

--- Monster/Monster.gd
+++ Monster/Monster.gd
@@ -6,8 +6,11 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 @export var turnSpeed = 4.0
+@export var damageInt = 1.0
 
 var player
+var damageTimer = 0.0
+var isPlayerIn = false
 
 func _ready():
 	# get player node
@@ -20,3 +23,25 @@ func _physics_process(delta):
 	var velocity = (navigation.get_next_path_position() - transform.origin).normalized() * SPEED * delta
 	move_and_collide(velocity)
 	
+	if isPlayerIn:
+		damagePlayer(delta)
+	
+
+
+func _on_attack_region_body_entered(body):
+	if body.is_in_group("Player"):
+		isPlayerIn = true
+		print("Player in range")
+
+func _on_attack_region_body_exited(body):
+	if body.is_in_group("Player"):
+		isPlayerIn = false
+		print("Player out of range")
+	
+
+func damagePlayer(delta):
+	damageTimer -= delta
+	if damageTimer <= 0:
+		# reset timer
+		damageTimer = damageInt
+		
