b1387d62dfa96eb7ee58d13bdfb88b311313a3ba
Reivax2003 <santiago25@up.edu>
2024-11-03 10:39:25 -0800

    UI changes

--- campFire/safeArea.gd
+++ campFire/safeArea.gd
@@ -105,11 +105,11 @@ func _process(delta):
 			add_stick()
 			
 		# Apply healing over time if the player is inside the radius
-		var ins = player.getInside()
-		if ins and player and is_instance_valid(player):
-			if player.health <= 3:
-				player.health += healing_rate * delta
-				#print("Player healed by", healing_rate * delta)
-				print("Players health is now", player.health)
+		#var ins = player.getInside()
+		#if ins and player and is_instance_valid(player):
+		#	if player.health <= 3:
+		#		player.heal(healing_rate * delta)
+		#		#print("Player healed by", healing_rate * delta)
+		#		print("Players health is now", player.health)
 			#else:
 				#print("Player already at max health")
